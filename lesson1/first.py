ebd84919b4fed0e9abc33879ac3ce0ff09c5f835
Your Name <you@example.com>
2020-03-26 21:31:14 +0600

    aaa

---
+++ lesson1/first.py
@@ -0,0 +1,5 @@
+n1 = input()
+n2 = input()
+n3 = int(n1)
+n4 = int(n2)
+print (n3 + n4)
---
+++ lesson1/forth.py
@@ -0,0 +1,7 @@
+a=int(input())
+b=int(input())
+c=int(input())
+if a>b and a>c and b>c: print(a-c)
+if b>a and b>c and a>c: print(b-c)
+if c>a and c>b and b>a: print(c-a)
+
